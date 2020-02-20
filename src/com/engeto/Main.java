7ec4411afe8e52f285db65f57a4dc16ff891f24b
Petra Peskova <petra.peskova@centrum.cz>
2020-02-20 19:35:09 +0100

    more_lovely_story

--- src/com/engeto/Main.java
+++ src/com/engeto/Main.java
@@ -16,9 +16,11 @@ public class Main {
         String name = scanner.nextLine();
 
         System.out.println("What is their job?");
+
         String job = scanner.nextLine();
+
         System.out.println("Here is the story: \n" +
-            "Once upon a time there was " + name + ", who was " + job + ". \n" +
+                "Once upon a time there was " + name + ", who was " + job + ". \n" +
                 "On the way to work, " + name + " reflected on life. \n" +
                 "Perhaps " + name + " will not be " + job + " forever.");
 
